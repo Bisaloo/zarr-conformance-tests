7137f1a886332b598f05cd6305f4ab02b3bfd6a0
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-10-27 15:20:48 +0100

    Add tests for more data types

--- tests.sh
+++ tests.sh
@@ -4,3 +4,23 @@
   run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/int64.zarr
   [ "$status" -eq 0 ]
 }
+
+@test "read int32" {
+  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/int32.zarr
+  [ "$status" -eq 0 ]
+}
+
+@test "read float64" {
+  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/float64.zarr
+  [ "$status" -eq 0 ]
+}
+
+@test "read float32" {
+  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/float32.zarr
+  [ "$status" -eq 0 ]
+}
+
+@test "read bool" {
+  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/bool.zarr
+  [ "$status" -eq 0 ]
+}
