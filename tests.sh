9da63da1b350155d1d1fb09f304e5461a6c549ec
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-11-24 15:20:58 +0100

    Add v3 test to illustrate failure

--- tests.sh
+++ tests.sh
@@ -1,26 +1,31 @@
 #! /bin/env bash
 
-@test "read int64" {
+@test "read int64 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int64.zarr
   [ "$status" -eq 0 ]
 }
 
-@test "read int32" {
+@test "read int32 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32.zarr
   [ "$status" -eq 0 ]
 }
 
-@test "read float64" {
+@test "read int32 - v3" {
+  run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32_v3.zarr
+  [ "$status" -eq 0 ]
+}
+
+@test "read float64 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float64.zarr
   [ "$status" -eq 0 ]
 }
 
-@test "read float32" {
+@test "read float32 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float32.zarr
   [ "$status" -eq 0 ]
 }
 
-@test "read bool" {
+@test "read bool - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/bool.zarr
   [ "$status" -eq 0 ]
 }
