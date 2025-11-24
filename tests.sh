3eabe131aea97b9b7e72c7f1c7e289f6e2065d46
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-11-24 14:03:35 +0100

    Use array_path instead of array_name

--- tests.sh
+++ tests.sh
@@ -1,26 +1,26 @@
 #! /bin/env bash
 
 @test "read int64" {
-  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/int64.zarr
+  run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int64.zarr
   [ "$status" -eq 0 ]
 }
 
 @test "read int32" {
-  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/int32.zarr
+  run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32.zarr
   [ "$status" -eq 0 ]
 }
 
 @test "read float64" {
-  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/float64.zarr
+  run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float64.zarr
   [ "$status" -eq 0 ]
 }
 
 @test "read float32" {
-  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/float32.zarr
+  run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float32.zarr
   [ "$status" -eq 0 ]
 }
 
 @test "read bool" {
-  run $ZARR_CLI --array_name=$ZARR_CONFORMANCE_DATA/bool.zarr
+  run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/bool.zarr
   [ "$status" -eq 0 ]
 }
