63d1aa81977a03fc2f986ee3a89025916b9e2603
brokkoli71 <brokkoli71b@gmail.com>
2026-02-07 12:57:43 +0100

    verbose output

--- tests.sh
+++ tests.sh
@@ -4,30 +4,48 @@ set -eu
 
 @test "read int64 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int64.zarr
+  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int64.zarr" >&3
+  echo "Exit status: $status" >&3
+  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read int32 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32.zarr
+  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32.zarr" >&3
+  echo "Exit status: $status" >&3
+  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read int32 - v3" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32_v3.zarr
+  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32_v3.zarr" >&3
+  echo "Exit status: $status" >&3
+  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read float64 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float64.zarr
+  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float64.zarr" >&3
+  echo "Exit status: $status" >&3
+  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read float32 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float32.zarr
+  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float32.zarr" >&3
+  echo "Exit status: $status" >&3
+  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read bool - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/bool.zarr
+  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/bool.zarr" >&3
+  echo "Exit status: $status" >&3
+  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
