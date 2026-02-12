021c440a1df4c775741ee17a9b4a7386bd11bd5d
Hugo Gruson <git@hugogruson.fr>
2026-02-12 14:00:34 +0100

    Make pr 16 changes less verbose

--- tests.sh
+++ tests.sh
@@ -20,32 +20,20 @@ set -eu
 
 @test "read int32 - v3" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32_v3.zarr
-  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/int32_v3.zarr" >&3
-  echo "Exit status: $status" >&3
-  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read float64 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float64.zarr
-  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float64.zarr" >&3
-  echo "Exit status: $status" >&3
-  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read float32 - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float32.zarr
-  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/float32.zarr" >&3
-  echo "Exit status: $status" >&3
-  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
 
 @test "read bool - v2" {
   run $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/bool.zarr
-  echo "Command: $ZARR_CLI --array_path=$ZARR_CONFORMANCE_DATA/bool.zarr" >&3
-  echo "Exit status: $status" >&3
-  echo "Output: $output" >&3
   [ "$status" -eq 0 ]
 }
